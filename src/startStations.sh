860395e962a439b31f466a95099a4f9cb1d9a061
Florian Weiß <weiss.florian.ws@web.de>
2018-12-11 16:07:42 +0100

    linux update

--- src/startStations.sh
+++ src/startStations.sh
@@ -49,7 +49,7 @@ teamNo="2"
 # Example:    dataSource="~/somewhere/DataSource"
 #         or  dataSource="java -cp . datasource.DataSource"
 ########################################################################################################
-dataSource="java -cp . java/vessel3/Vessel"
+dataSource="java -cp . vessel3.Vessel"
 
 ########################################################################################################
 # TODO: Enter your station's start command.
